ba6e9f2d90b1f9e4efd61cef5ad83e33ff0a9c4d
Muhammadalihaidera <152718805+Muhammadalihaidera@users.noreply.github.com>
2024-01-09 21:06:56 +0500

    haider

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ['Freelancer, I do off-page SEO and web development.', 'Freelancer, I do off-page SEO and web development.', 'Freelancer, I do off-page SEO and web development.',],
+    typeSpeed: 50,
+    BackSpeed:50,
+    backDelay:1000,
+    loop:true,
+  });
